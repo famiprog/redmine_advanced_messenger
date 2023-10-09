f5c5db20ef276dacab3c873777c2af6ee3c9d8e6
mircea-negreanu <mircea.negreanu@crispico.com>
2023-10-09 17:11:32 +0300

    #32296 - increase version to 1.0.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_advanced_messenger do
   name 'Redmine Advanced Messenger plugin'
   author 'famiprog'
   description 'For detailed documentation see the link below.'
-  version '0.0.1'
+  version '1.0.0'
   url 'https://github.com/famiprog/redmine-advanced-messenger'
   author_url 'https://github.com/famiprog'
 
