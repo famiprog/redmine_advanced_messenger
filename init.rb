fec668d348fec7839bedfd89d35e8b6ef4396fde
Daniela Buzatu <daniela.buzatu@crispico.com>
2023-10-27 17:16:43 +0300

    Fix javascript injection via note content problem

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_advanced_messenger do
   name 'Redmine Advanced Messenger plugin'
   author 'famiprog'
   description 'For detailed documentation see the link below.'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/famiprog/redmine-advanced-messenger'
   author_url 'https://github.com/famiprog'
 
