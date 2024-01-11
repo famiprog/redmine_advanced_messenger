f1bab9697a9fb252561cdcd0245e8e8b1d089082
Daniela Mateescu <daniela.mateescu@crispico.com>
2024-01-11 16:25:32 +0200

    Dana rm33147 modificari pentru v1.2.0 (#12)
    * Ignored message status + ignore all buttons
    
    * no delay when showing the the notification popup
    
    * review changes
    
    ---------
    
    Co-authored-by: Daniela Buzatu <daniela.buzatu@crispico.com>

--- app/controllers/advanced_messenger_controller.rb
+++ app/controllers/advanced_messenger_controller.rb
@@ -1,16 +1,26 @@
 class AdvancedMessengerController < ApplicationController
 
   def update_journal_read_by_users
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
     @journal = Journal.find_by(id: params[:id])
     update_entity_read_by_users(@journal, "journal", params[:id], params[:read_by_users],
-                               lambda {|journal| return journal.journalized.visible? && (!journal.private_notes? || User.current.allowed_to?(:view_private_notes, journal.journalized.project) || journal.user.id == User.current.id)},
+                               method(:is_journal_visible),
                                lambda {|journal| @unread_notification_statuses = helpers.getUnreadJournalsStatus(journal.issue.visible_journals_with_index)})
   end
 
   def update_message_read_by_users
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
     @message = Message.find_by(id: params[:id])
     update_entity_read_by_users(@message, "message", params[:id], params[:read_by_users],
-                                lambda {|message| return message.visible?()},
+                                method(:is_message_visible),
                                 lambda {|message| 
                                         @replies =  @message.root.children
                                                                       .includes(:author, :attachments, {:board => :project})
@@ -24,6 +34,7 @@ class AdvancedMessengerController < ApplicationController
   # 0 -> unread
   # 1 -> read
   # 2 -> read but collapsed
+  # private helper method
   def update_entity_read_by_users(entity, entity_name, id, read_value, is_visible, response_setup)
     if entity == nil
       Rails.logger.error("Could not find  #{entity_name} #{id}")
@@ -37,7 +48,7 @@ class AdvancedMessengerController < ApplicationController
       return
     end
     read_by_users = JSON.parse(entity.read_by_users);
-    if not ["0", "1", "2"].include? read_value
+    if not ["0", "1", "2", "3"].include? read_value
       Rails.logger.error("#{read_value} is not a valid read status for a user")
       render_404
       return
@@ -55,6 +66,7 @@ class AdvancedMessengerController < ApplicationController
     end
   end
 
+  # private helper method
   def set_current_user_new_read_collapsed_value(read_by_users, new_value)
     if read_by_users[User.current.id.to_s] == nil &&  new_value == "2"
       read_by_users[User.current.id.to_s] = {collapsed: 1}
@@ -63,5 +75,71 @@ class AdvancedMessengerController < ApplicationController
     else  
       read_by_users[User.current.id.to_s] = {read: new_value.to_i, date: DateTime.now}
     end
+  end
+
+  def ignore_all_unread_issues_notes
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
+    unread_issues_notifications = Journal.where("notes != '' AND notes IS NOT NULL AND read_by_users ILIKE ?", '%"' + User.current.id.to_s + '":{"read":0%');
+    ignore_all_unread_entities(unread_issues_notifications, method(:is_journal_visible))
+  end
+
+  def ignore_all_unread_issues_notes_for_an_issue
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
+    unread_issue_notifications = Journal.where("notes != '' AND notes IS NOT NULL AND journalized_id = ? AND read_by_users ILIKE ?", params[:issue_id], '%"' + User.current.id.to_s + '":{"read":0%');
+    ignore_all_unread_entities(unread_issue_notifications, method(:is_journal_visible))
+  end
+
+  def ignore_all_unread_forum_messages
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
+    unread_forum_messages = Message.where("read_by_users ILIKE ?", '%"' + User.current.id.to_s + '":{"read":0%');
+    ignore_all_unread_entities(unread_forum_messages, method(:is_message_visible))
+  end
+
+  def ignore_all_unread_forum_messages_for_a_topic
+    if !User.current.logged?
+      Rails.logger.error("No logged in user")
+      render_403
+      return
+    end
+    unread_messages_for_topic = Message.where("(id = ? OR parent_id = ?) AND read_by_users ILIKE ?", params[:topic_id], params[:topic_id], '%"' + User.current.id.to_s + '":{"read":0%');
+    ignore_all_unread_entities(unread_messages_for_topic, method(:is_message_visible))
+  end
+
+  # private helper method
+  def ignore_all_unread_entities(unread_entities, is_visible)
+    unread_entities.each_with_index do |unread_entity, index|
+      if is_visible.call(unread_entity) 
+        read_by_users = JSON.parse(unread_entity.read_by_users) 
+        read_by_users[User.current.id.to_s]["read"] = 3
+        unread_entity.read_by_users = read_by_users.to_json
+        unread_entity.save
+      end
+    end
+
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
+  end
+
+  # private helper method
+  def is_journal_visible (journal)
+    return journal.journalized.visible? && (!journal.private_notes? || User.current.allowed_to?(:view_private_notes, journal.journalized.project) || journal.user.id == User.current.id)
+  end
+
+  # private helper method
+  def is_message_visible (message)
+    return message.visible?();  
   end
 end
