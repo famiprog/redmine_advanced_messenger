200e0da5808846764453144debb858cb2e7fe4bc
Alin Bonciu <alin.bonciu@resonate-mp4.com>
2024-09-11 08:55:50 +0300

    fixed aliasing, patches working as expected

--- lib/patches/mailer_patch.rb
+++ lib/patches/mailer_patch.rb
@@ -1,31 +1,39 @@
+require_dependency 'mailer'
+
 module Patches
   module MailerPatch
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
-        alias_method :original_issue_edit, :issue_edit
-        alias_method :original_message_posted, :message_posted
+        alias_method :super_issue_edit, :issue_edit
+        alias_method :issue_edit, :patched_issue_edit
+        
+        alias_method :super_message_posted, :message_posted
+        alias_method :message_posted, :patched_message_posted
       end
     end
 
-    module InstanceMethods
-      def issue_edit(journal)
-        user_roles = journal.user.roles.map(&:id).map(&:to_s)
-        allowed_roles = Setting.plugin_redmine_advanced_messenger[:allowed_roles] || []
-      
-        if (user_roles & allowed_roles).empty?
-          original_issue_edit(journal)
+    module InstanceMethods      
+      def patched_issue_edit(user, journal)
+        unless user_has_allowed_role?(user)
+          super_issue_edit(user, journal)
         end
       end
-      
-      def message_posted(message)
-        user_roles = message.user.roles.map(&:id).map(&:to_s)
-        allowed_roles = Setting.plugin_redmine_advanced_messenger[:allowed_roles] || []
-      
-        if (user_roles & allowed_roles).empty?
-          original_message_posted(message)
+
+      def patched_message_posted(user, message)
+        unless user_has_allowed_role?(user)
+          super_message_posted(user, message)
         end
       end
+
+      private
+
+      def user_has_allowed_role?(user)
+        user_roles = user.roles.map(&:id).map(&:to_s)
+        allowed_roles = (Setting.plugin_redmine_advanced_messenger[:allowed_roles] || []).map(&:to_s)
+        (user_roles & allowed_roles).any?
+      end      
+      
     end
   end
 end
