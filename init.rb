914d64c0136b39d0553c45a9abeeaad66990fd4f
Alin Bonciu <alin.bonciu@resonate-mp4.com>
2024-09-19 12:14:05 +0300

    restrict email exclusion logic to project-specific roles + renaming

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_advanced_messenger do
                         :show_unread_notifications => '1', 
                         :disable_fix_for_scroll_to_anchor => '0',
                         :unread_notifications_update_interval => 60,
-                        :allowed_roles => [] }, 
+                        :roles_not_sent_email => [] }, 
             :partial => 'settings/advanced_messenger_settings'
 
   # need the helper to be available in _issue_notifications.html.erb 
--- lib/patches/mailer_patch.rb
+++ lib/patches/mailer_patch.rb
@@ -15,23 +15,23 @@ module Patches
 
     module InstanceMethods      
       def patched_issue_edit(user, journal)
-        unless user_has_allowed_role?(user)
+        unless user_excluded_from_email?(user, journal.issue.project)
           super_issue_edit(user, journal)
         end
       end
 
       def patched_message_posted(user, message)
-        unless user_has_allowed_role?(user)
+        unless user_excluded_from_email?(user, message.project)
           super_message_posted(user, message)
         end
       end
 
       private
 
-      def user_has_allowed_role?(user)
-        user_roles = user.roles.map(&:id).map(&:to_s)
-        allowed_roles = (Setting.plugin_redmine_advanced_messenger[:allowed_roles] || []).map(&:to_s)
-        (user_roles & allowed_roles).any?
+      def user_excluded_from_email?(user, project)
+        user_roles_in_project = user.roles_for_project(project).map(&:id).map(&:to_s)
+        roles_not_sent_email = (Setting.plugin_redmine_advanced_messenger[:roles_not_sent_email] || []).map(&:to_s)
+        (user_roles_in_project & roles_not_sent_email).any?
       end      
       
     end
