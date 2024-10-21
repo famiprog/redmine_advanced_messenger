b8cef3d420e7c0b9bc2c3373bc6e157902bfc2e3
Mircea Negreanu <mircea.negreanu@crispico.com>
2024-10-21 18:04:15 +0300

    #35383 - correctly block mails with only comments for Issue updated config as well

--- lib/patches/mailer_patch.rb
+++ lib/patches/mailer_patch.rb
@@ -2,6 +2,12 @@ require_dependency 'mailer'
 
 module Patches
   module MailerPatch
+    # foce this helper, since apparently the patcher does not inherit the 
+    # helpers from parent, or the mailer (which is not a controller) behaves
+    # differently. The end result is that helpers() function is not available 
+    # in the patch (and probably neither in mailer?)
+    include IssuesHelper
+
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
@@ -15,15 +21,22 @@ module Patches
 
     module InstanceMethods      
       def patched_issue_edit(user, journal)
+        details = []
+        # get the details_to_string only if we have notifications on all the modifications from a issue
+        if Setting.notified_events.include?('issue_updated')
+          details = details_to_strings(journal.visible_details, true, :only_path => false)
+        end
         # We want "Roles to exclude from email notifications" to only block notifications for notes added.
         # But when adding notes, the user can make other modifications, which should send notification by email 
         # (depending on the redmine configuration)
-        # So we need to copy the code from Journal that checks everything before sending a notification
-        # Note that if the user selected to be notified on all updates, the blocking done for notes add won't work anymore.
-        # (this is a shortcoming because it is difficult to verify that only a note was added. The code will involve calling
-        # the details_to_strings  method - the same one called during the rendering of the email message - which might not be worth it)
+        # So we need to copy the code from Journal that checks everything before sending a notification.
+        # If the user configured to be notified on all updates, we use the user_excluded_from_email function (that
+        # is also used in template) to see if we have fields completed. The second parameter (no_html) is set to true
+        # because 1/ we are not intereseted in html and the time needed to fill it and 2/ apaprently there is a problem
+        # when calling helper from mailer. We had to force including IssuesHelper (which contains the function) but 
+        # the html code also calls other functions from other helpers (e.g. h()) which is not available
         if 
-          Setting.notified_events.include?('issue_updated') ||
+          Setting.notified_events.include?('issue_updated') && (details.size > 0  || !user_excluded_from_email?(user, journal.issue.project)) || 
           (Setting.notified_events.include?('issue_status_updated') && journal.new_status.present?) ||
           (Setting.notified_events.include?('issue_assigned_to_updated') && journal.detail_for_attribute('assigned_to_id').present?) ||
           (Setting.notified_events.include?('issue_priority_updated') && journal.new_value_for('priority_id').present?) ||
