dcf6f17e384d5162e224ab908d669e75e26243e6
Teodor Duta <teodor.duta@resonate-mp4.com>
2024-11-07 16:41:50 +0200

    #35468 - move behavior from SW to plugin JS

--- assets/javascripts/redmine_advanced_messenger.js
+++ assets/javascripts/redmine_advanced_messenger.js
@@ -1,6 +1,5 @@
 const PWA_START_URL = "/my/page";
 const PWA_BADGE_VALUE = "PWA_BADGE_VALUE";
-const TIMER_CLOSING_OLD_NOTIFICATIONS = 5; // seconds
 
 // register service worker
 function registerPWAServiceWorker() {
@@ -28,6 +27,53 @@ function registerPWAServiceWorker() {
             window.open(e.target.href);
         }
     })
+
+    /**
+     * Receive a message from the service worker when the button 'markAndSubmitAnswer' is clicked with the notification data. 
+     * In this way is easier to make the corresponding API requests from here because Rails.ajax() supplies authorization and CSRF token automatically.
+     */
+    new BroadcastChannel('markAndSubmitAnswer').addEventListener('message', event => {
+        const eventNotification = event.data;
+        const isForum = eventNotification.notificationData.url.includes("boards");
+        markNoteOrMessageAsRead(eventNotification.notificationData.notificationId, isForum);
+        submitAnswerToIssueOrForum(eventNotification.reply, eventNotification.notificationData.taskId, isForum, eventNotification.notificationData.parentTaskId, eventNotification.notificationData.taskSubject)
+    });
+}
+
+/**
+ * Create a note/message for a issue/forum.
+ * @param answer - note/message content mandatory.
+ * @param taskId - issue/forum id mandatory.
+ * @param isForum - check what url to use, by default issue url.
+ * @param parentTaskId - if is forum mandatory.
+ * @param taskSubject - if is forum mandatory.
+ */
+function submitAnswerToIssueOrForum(answer, taskId, isForum, parentTaskId, taskSubject) {
+    if ((!answer || !taskId)) {
+        return;
+    }
+    if (isForum && (!parentTaskId || !taskSubject)) {
+        return;
+    }
+    Rails.ajax({
+        // very important to use '.json' endpoint (for issue) because the update can be done with the XML type also, so the request will be redirected several times resulting in more API calls => more duplicated notes
+        url: window.location.origin + (isForum ? `/boards/${parentTaskId}/topics/${taskId}/replies` : `/issues/${taskId}.json`),
+        type: isForum ? "POST" : "PUT",
+        beforeSend: function (xhr, options) {
+            // very important to set 'Content-Type', otherwise the server cannot interpret correctly the body received and it will respond with 500 Internal server error
+            xhr.setRequestHeader("Content-Type", "application/json");
+            // set the data (body) in here because if I set as a option then the 'Content-Type' will be automatically set to 'application/x-www-form-urlencoded; charset=UTF-8' and I cannot override it
+            options.data = JSON.stringify(isForum ? { reply: { content: answer, subject: taskSubject } } : { issue: { notes: answer } });
+            return true;
+        }
+    });
+}
+
+function markNoteOrMessageAsRead(messageId, isForum) {
+    if (!messageId) {
+        return;
+    }
+    Rails.ajax({ url: window.location.origin + `/advanced_messenger/${messageId}/1/update_${isForum ? "message" : "journal"}_read_by_users`, type: "POST" });
 }
 
 // c.f https://stackoverflow.com/a/41749865 check manifest display, needs to update this method if it will be change
@@ -35,7 +81,7 @@ function checkPWA() {
     return window.matchMedia('(display-mode: standalone)').matches
 }
 
-function refreshPWA(badgeValue, notifications, userApiKey, actions) {
+function refreshPWA(badgeValue, notifications, notificationActions) {
     if (!checkPWA()) {
         return;
     }
@@ -65,12 +111,7 @@ function refreshPWA(badgeValue, notifications, userApiKey, actions) {
         } else {
             notifications.forEach(function (notification) {
                 setTimeout(function () {
-                    var data = {
-                        notificationInfo: notification,
-                        xCsrfToken: document.querySelector('meta[name="csrf-token"]').content,
-                        apiKey: userApiKey,
-                    }
-                    showNotification(notification.title, notification.message, data, actions);
+                    showNotification(notification.title, notification.message, notification, notificationActions);
                 }, delay);
                 delay += 1000; // Increase the delay by 1 second for each subsequent notification
                 // Without this delay, notifications would be sent one after another too quickly, causing only the last one to be received
--- assets/pwa/service-worker.js
+++ assets/pwa/service-worker.js
@@ -1,9 +1,4 @@
 const CACHE_NAME = "RedmineAdvancedMessenger"
-// Request headers keys
-const REDMINE_API_KEY_HEADER = "X-Redmine-API-Key";
-const CSRF_TOKEN_HEADER = "x-csrf-token";
-const CONTENT_TYPE_HEADER = "Content-Type";
-const CONTENT_TYPE_JSON = "application/json";
 
 self.addEventListener("install", event =>
     event.waitUntil(
@@ -29,48 +24,19 @@ self.addEventListener('fetch', (event) => {
 
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
-    const notificationData = event.notification.data;
     if (event.action == 'openMessage') {
-        var url = new URL(notificationData.notificationInfo.url, self.location.origin);
-        url.searchParams.append("markNoteAsRead", notificationData.notificationInfo.notificationId);
+        var url = new URL(event.notification.data.url, self.location.origin);
+        url.searchParams.append("markNoteAsRead", event.notification.data.notificationId);
         event.waitUntil(openUrlInBrowserWindow(url.href));
     } else if (event.action == "markAndSubmitAnswer") {
-        var headers = { [CSRF_TOKEN_HEADER]: notificationData.xCsrfToken, [REDMINE_API_KEY_HEADER]: notificationData.apiKey, [CONTENT_TYPE_HEADER]: CONTENT_TYPE_JSON };
-        const isForum = notificationData.notificationInfo.url.includes("boards");
-        markAsRead(notificationData.notificationInfo.notificationId, isForum, headers);
-        if (event.reply) {
-            submitAnswer(event.reply, notificationData.notificationInfo.taskId, notificationData.notificationInfo.parentTaskId, notificationData.notificationInfo.taskSubject, isForum, headers)
-        };
+        // post a message to the channel which will be received in the main page, in this way we don't need to deal about the authorization for the requests
+        event.waitUntil(async () => new BroadcastChannel('markAndSubmitAnswer').postMessage({ reply: event.reply, notificationData: event.notification.data }));
     } else if (!event.action) {
-        event.waitUntil(openUrlInBrowserWindow(notificationData.notificationInfo.url));
+        event.waitUntil(openUrlInBrowserWindow(event.notification.data.url));
     } else {
         console.log(`Unknown action clicked: '${event.action}'`);
     }
 });
-
-function submitAnswer(answer, taskId, parentTaskId, taskSubject, isForum, requestHeaders) {
-    var body = isForum ? `{"reply":{"content":"${answer}","subject":"${taskSubject}"}}` : `{"issue":{"notes":"${answer}"}}`;
-    // very important to use '.json' endpoint because the update can be done with the XML type also, so the request will be redirected several times resulting in more API calls => more notes
-    return executeAPIRequest(self.location.origin + (isForum ? `/boards/${parentTaskId}/topics/${taskId}/replies` : `/issues/${taskId}.json`), isForum ? "POST" : "PUT", requestHeaders, body);
-}
-
-function markAsRead(messageId, isForum, requestHeaders) {
-    var endpoint = `/advanced_messenger/${messageId}/1/update_${isForum ? "message" : "journal"}_read_by_users`;
-    return executeAPIRequest(self.location.origin + endpoint, "POST", requestHeaders);
-}
-
-function executeAPIRequest(url, methodType, headers, body, successCallback, errorCallback) {
-    return fetch(url, { method: methodType, headers: headers, body: body })
-        .then(response => {
-            if (successCallback) {
-                successCallback(response);
-            }
-        }).catch(error => {
-            if (errorCallback) {
-                errorCallback(error);
-            }
-        });
-}
 
 function openUrlInBrowserWindow(url) {
     const urlToOpen = new URL(url, self.location.origin).href;
